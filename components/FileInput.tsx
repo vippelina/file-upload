4ab393cd264ce5094704648e4c71559a0a9b640c
Vibeke Tengroth <vibeketengroth@gmail.com>
2022-07-08 16:54:00 +0200

    initial commit

---
+++ components/FileInput.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+interface fileInputProps {}
+
+const FileInput: React.FC<fileInputProps> = ({}) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [selectedFiles, setSelectedFiles] = React.useState<
+    HTMLInputElement["files"] | null
+  >(null);
+
+  console.log("vippe selectedFiles", selectedFiles);
+  const onFormSubmit = async (e: React.FormEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const formData = new FormData();
+    if (!selectedFiles?.length) return;
+
+    // prepare payload
+    formData.append("file", selectedFiles[0]);
+
+    const response = await fetch("/api/hello", {
+      method: "POST",
+      body: formData,
+    });
+    // TODO samla på dig all data o skicka till servern
+  };
+
+  return (
+    <>
+      <form>
+        <p>State:</p>
+        <p>Loading: {isLoading ? "true" : "false"}</p>
+        <p>SelectedFiles: {selectedFiles?.length}</p>
+        <input type="file" onChange={(e) => setSelectedFiles(e.target.files)} />
+        <input type="submit" onClick={onFormSubmit} />
+      </form>
+    </>
+  );
+};
+
+export default FileInput;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,57 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-
+import type { NextApiRequest, NextApiResponse } from "next";
+import formidable, { File } from "formidable";
+import fs from "fs";
 type Data = {
-  name: string
+  name: string;
+};
+
+interface ProcessedFile {
+  name?: string;
+  file?: File;
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  if (req.method === "POST") {
+    // Process a POST request
+    console.log("vippe in i post");
+    console.log("dirname", __dirname);
+
+    // parse the formData with formidable
+    const form = new formidable.IncomingForm();
+    const file: ProcessedFile = { name: "" };
+    form.on("file", (name, incomingFile) => {
+      console.log("got file!");
+      file.name = name;
+      file.file = incomingFile;
+      const oldPath = incomingFile.filepath;
+      const newPath = `./public/uploads/${incomingFile.originalFilename}`;
+      console.log("vippe old path", oldPath);
+      console.log("vippe got newPath", newPath);
+      fs.rename(oldPath, newPath, function (err) {
+        console.log("vippe got err", err);
+        if (err) throw err;
+        console.log("bubba!");
+        console.log("newpath", newPath);
+      });
+    });
+    form.parse(req, () => {
+      //
+    });
+    form.on("end", () => {
+      console.log("vippe klar!", file);
+    });
+    //res.status(200).json({ name: "John Doe" });
+  } else {
+    // Handle any other HTTP method
+  }
 }
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
